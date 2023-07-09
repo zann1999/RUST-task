ec0b65683da7a61120bbe872683be49f75607389
zann1999 <93304736+zann1999@users.noreply.github.com>
2023-07-09 12:31:08 +0700

    Add files via upload

---
+++ ownership-borrowing/target/debug/deps/ownership_borrowing-699ac4c4c6f0f9d2.d
@@ -0,0 +1,5 @@
+C:\Users\ADMIN\Downloads\ownership-borrowing\target\debug\deps\ownership_borrowing-699ac4c4c6f0f9d2.rmeta: src\lib.rs
+
+C:\Users\ADMIN\Downloads\ownership-borrowing\target\debug\deps\ownership_borrowing-699ac4c4c6f0f9d2.d: src\lib.rs
+
+src\lib.rs:
---
+++ ownership-borrowing/target/debug/deps/ownership_borrowing-8fa2a3b6c3c4371c.d
@@ -0,0 +1,5 @@
+C:\Users\ADMIN\Downloads\ownership-borrowing\target\debug\deps\ownership_borrowing-8fa2a3b6c3c4371c.rmeta: src\lib.rs
+
+C:\Users\ADMIN\Downloads\ownership-borrowing\target\debug\deps\ownership_borrowing-8fa2a3b6c3c4371c.d: src\lib.rs
+
+src\lib.rs:
