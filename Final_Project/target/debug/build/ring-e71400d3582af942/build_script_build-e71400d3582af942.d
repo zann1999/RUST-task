666ecbacb3c3b22a6a025d9605e98186a1b53be9
zann1999 <93304736+zann1999@users.noreply.github.com>
2023-07-22 17:00:10 +0700

    Add files via upload

---
+++ Final_Project/target/debug/build/ring-e71400d3582af942/build_script_build-e71400d3582af942.d
@@ -0,0 +1,5 @@
+C:\Users\ADMIN\Downloads\final-project\target\debug\build\ring-e71400d3582af942\build_script_build-e71400d3582af942.exe: C:\Users\ADMIN\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ring-0.16.20\build.rs
+
+C:\Users\ADMIN\Downloads\final-project\target\debug\build\ring-e71400d3582af942\build_script_build-e71400d3582af942.d: C:\Users\ADMIN\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ring-0.16.20\build.rs
+
+C:\Users\ADMIN\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ring-0.16.20\build.rs:
---
+++ Final_Project/target/debug/build/winapi-a3a56b7062bbc1c1/build_script_build-a3a56b7062bbc1c1.d
@@ -0,0 +1,5 @@
+C:\Users\ADMIN\Downloads\final-project\target\debug\build\winapi-a3a56b7062bbc1c1\build_script_build-a3a56b7062bbc1c1.exe: C:\Users\ADMIN\.cargo\registry\src\index.crates.io-6f17d22bba15001f\winapi-0.3.9\build.rs
+
+C:\Users\ADMIN\Downloads\final-project\target\debug\build\winapi-a3a56b7062bbc1c1\build_script_build-a3a56b7062bbc1c1.d: C:\Users\ADMIN\.cargo\registry\src\index.crates.io-6f17d22bba15001f\winapi-0.3.9\build.rs
+
+C:\Users\ADMIN\.cargo\registry\src\index.crates.io-6f17d22bba15001f\winapi-0.3.9\build.rs:
